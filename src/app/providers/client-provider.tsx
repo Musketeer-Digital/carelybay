ad615e56c735b02b5de2345d74b1f9174f598ef4
vworo <victorworobec@gmail.com>
2025-02-01 23:10:13 +1100

    move logo and name banner to clientprovider

--- src/app/providers/client-provider.tsx
+++ src/app/providers/client-provider.tsx
@@ -3,7 +3,8 @@
 import * as React from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../theme";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, Box, Typography } from "@mui/material";
+import Image from "next/image";
 
 export default function ClientProvider({
   children,
@@ -11,6 +12,17 @@ export default function ClientProvider({
   return (
     <>
       <ThemeProvider theme={theme}>
+        {/* Logo and Name */}
+        <Box sx={{ display: "flex", alignItems: "center", position: "absolute", gap: "1em", margin: "1em", marginLeft: "2em" }}>
+          <Image
+            src="https://placehold.co/28x28"
+            alt="Logo"
+            width={28}
+            height={28}
+            unoptimized
+          />
+          <Typography variant="h5">Logotype</Typography>
+        </Box>
         <CssBaseline />
         {children}
       </ThemeProvider>
