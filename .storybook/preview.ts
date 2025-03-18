afbcfa72ed9ad237d2c659f19144d8017f81069c
Robinson Lam <robinsonlam94@gmail.com>
2025-03-18 23:05:29 +1100

    fix storybook theming

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,10 +1,10 @@
 import type { Preview } from "@storybook/react";
 
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
 
 /* TODO: update import for your custom Material UI themes */
-import { lightTheme, darkTheme } from '../path/to/themes';
+import { theme } from "../src/app/theme";
 
 const preview: Preview = {
   parameters: {
@@ -21,27 +21,26 @@ export default preview;
 
 // .storybook/preview.js
 
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { withThemeFromJSXProvider } from "@storybook/addon-themes";
-import { theme } from "../src/app/theme";
-
 /* snipped for brevity */
 
-export const decorators = [withThemeFromJSXProvider({
-  themes: {
-    light: theme,
-    dark: theme,
-  },
-  defaultTheme: "light",
-  Provider: ThemeProvider,
-  GlobalStyles: CssBaseline,
-}), withThemeFromJSXProvider({
-  GlobalStyles: CssBaseline,
-  Provider: ThemeProvider,
-  themes: {
-    // Provide your custom themes here
-    light: lightTheme,
-    dark: darkTheme,
-  },
-  defaultTheme: 'light',
-})];
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      light: theme,
+      dark: theme,
+    },
+    defaultTheme: "light",
+    Provider: ThemeProvider,
+    GlobalStyles: CssBaseline,
+  }),
+  withThemeFromJSXProvider({
+    GlobalStyles: CssBaseline,
+    Provider: ThemeProvider,
+    themes: {
+      // Provide your custom themes here
+      light: theme,
+      dark: theme,
+    },
+    defaultTheme: "light",
+  }),
+];
