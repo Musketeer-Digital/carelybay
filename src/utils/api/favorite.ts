e93443365b8838b215b84ce33e9a38a6d08e422e
Arbaz71 <arbazqureshi.official@gmail.com>
2025-06-03 20:01:56 +0500

    updated endpoint

--- src/utils/api/favorite.ts
+++ src/utils/api/favorite.ts
@@ -3,7 +3,7 @@ import { apiRequest } from "../apiRequest";
 const FAVORITE_API = "/api/favorite";
 
 export const toggleFavorite = async (jobId: string, userId: string) => {
-  return apiRequest(`${FAVORITE_API}/toggle`, "PUT", { jobId, userId });
+  return apiRequest(`${FAVORITE_API}/favorite`, "PUT", { jobId, userId });
 };
 
 export const checkFavoriteStatus = async (jobId: string, userId: string) => {
