d00520f6118cc4ab9181d0eb057a05b71281fab9
Arbaz71 <arbazqureshi.official@gmail.com>
2025-06-04 18:50:09 +0500

    Removed hard coded checks

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -8,7 +8,7 @@ type UserState = {
 };
 
 export const useUserStore = create<UserState>((set) => ({
-  user: { _id: "6823858aea02370c792a2ec4" }, // or leave empty: user: undefined,
+  user: { _id: undefined },
   setUser: (user) => set({ user }),
   clearUser: () => set({ user: undefined }),
 }));
